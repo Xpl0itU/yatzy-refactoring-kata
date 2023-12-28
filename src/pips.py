8551cfdb1214ece697e0585cd9c30f54f9cc3c3e
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2023-12-28 14:04:16 +0100

    refactor(pips.py): change hardcoded values to range

--- src/pips.py
+++ src/pips.py
@@ -3,12 +3,7 @@ from enum import Enum, unique
 
 @unique
 class Pips(Enum):
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
+    ONE, TWO, THREE, FOUR, FIVE, SIX = range(1, 6 + 1)
 
     @classmethod
     def values(cls):
