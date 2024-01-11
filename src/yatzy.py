a430d7fbc8d1cf9ac77d000eb5009ce3ce230195
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-01-11 11:16:34 +0100

    refactor: make generic methods private

--- src/yatzy.py
+++ src/yatzy.py
@@ -21,39 +21,39 @@ class Yatzy:
         return 50
 
     @staticmethod
-    def n_dice(pip, *dice):
+    def __n_dice(pip, *dice):
         n = pip.value
         return dice.count(n) * n
 
     @classmethod
     def ones(cls, *dice):
-        return cls.n_dice(Pips.ONE, *dice)
+        return cls.__n_dice(Pips.ONE, *dice)
 
     @classmethod
     def twos(cls, *dice):
-        return cls.n_dice(Pips.TWO, *dice)
+        return cls.__n_dice(Pips.TWO, *dice)
 
     @classmethod
     def threes(cls, *dice):
-        return cls.n_dice(Pips.THREE, *dice)
+        return cls.__n_dice(Pips.THREE, *dice)
 
     @classmethod
     def fours(cls, *dice):
-        return cls.n_dice(Pips.FOUR, *dice)
+        return cls.__n_dice(Pips.FOUR, *dice)
 
     @classmethod
     def fives(cls, *dice):
-        return cls.n_dice(Pips.FIVE, *dice)
+        return cls.__n_dice(Pips.FIVE, *dice)
 
     @classmethod
     def sixes(cls, *dice):
-        return cls.n_dice(Pips.SIX, *dice)
+        return cls.__n_dice(Pips.SIX, *dice)
 
     def has_n_dice(self, pip):
-        return self.n_dice(pip, *self.dice)
+        return self.__n_dice(pip, *self.dice)
 
     @staticmethod
-    def n_pairs(n, *dice):
+    def __n_pairs(n, *dice):
         numbers_occurrence_count = {}
         for die in dice:
             if die not in numbers_occurrence_count:
@@ -79,15 +79,15 @@ class Yatzy:
     @classmethod
     def pair(cls, *dice):
         ONE_PAIR = Pips.ONE.value
-        return cls.n_pairs(ONE_PAIR, *dice)
+        return cls.__n_pairs(ONE_PAIR, *dice)
 
     @classmethod
     def two_pairs(cls, *dice):
         TWO_PAIRS = Pips.TWO.value
-        return cls.n_pairs(TWO_PAIRS, *dice)
+        return cls.__n_pairs(TWO_PAIRS, *dice)
 
     @classmethod
-    def n_of_a_kind(cls, n, *dice, exactly_n=False):
+    def __n_of_a_kind(cls, n, *dice, exactly_n=False):
         pip = cls.__biggest_pip_repeated(dice, n)
         if exactly_n:
             for pip in Pips.reversedValues():
@@ -98,11 +98,11 @@ class Yatzy:
 
     @classmethod
     def three_of_a_kind(cls, *dice):
-        return cls.n_of_a_kind(Pips.THREE.value, *dice)
+        return cls.__n_of_a_kind(Pips.THREE.value, *dice)
 
     @classmethod
     def four_of_a_kind(cls, *dice):
-        return cls.n_of_a_kind(Pips.FOUR.value, *dice)
+        return cls.__n_of_a_kind(Pips.FOUR.value, *dice)
 
     @classmethod
     def __biggest_pip_repeated(cls, dice, times):
@@ -114,20 +114,20 @@ class Yatzy:
         return list(filter(lambda pip: dice.count(pip) >= times, Pips.reversedValues()))
 
     @classmethod
-    def straight(cls, pip_set, *dice):
+    def __straight(cls, pip_set, *dice):
         return cls.chance(*dice) if not Pips.minus(pip_set) - set(dice) else 0
 
     @classmethod
     def small_straight(cls, *dice):
-        return cls.straight(Pips.SIX, *dice)
+        return cls.__straight(Pips.SIX, *dice)
 
     @classmethod
     def large_straight(cls, *dice):
-        return cls.straight(Pips.ONE, *dice)
+        return cls.__straight(Pips.ONE, *dice)
 
     @classmethod
     def full_house(cls, *dice):
-        two_of_a_kind = cls.n_of_a_kind(Pips.TWO.value, *dice, exactly_n=True)
+        two_of_a_kind = cls.__n_of_a_kind(Pips.TWO.value, *dice, exactly_n=True)
         three_of_a_kind = cls.three_of_a_kind(*dice)
         if two_of_a_kind and three_of_a_kind:
             return two_of_a_kind + three_of_a_kind
