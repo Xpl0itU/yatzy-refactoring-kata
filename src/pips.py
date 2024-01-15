490207156680bc4cb80ddf9961a156468b955ca5
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-01-15 10:37:46 +0100

    fix(pips.py): remove wrong type annotation

--- src/pips.py
+++ src/pips.py
@@ -7,5 +7,5 @@ class Pips(IntEnum):
     ONE, TWO, THREE, FOUR, FIVE, SIX = range(1, 6 + 1)
 
     @classmethod
-    def exclude_values(cls, *values_to_exclude) -> Set[int]:
+    def exclude_values(cls, *values_to_exclude):
         return set(filter(lambda pip: pip not in values_to_exclude, Pips))
